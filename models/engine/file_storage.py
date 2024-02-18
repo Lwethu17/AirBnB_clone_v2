62624b4778279e4605e3a216d438ebb058ef77b8
Lwethu17 <olwethuradebe18@gmail.com>
2024-02-18 09:53:28 -0800

    web flask

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,77 +1,70 @@
 #!/usr/bin/python3
-"""Defines a class FileStorage that serializes instances to a JSON file
-and deserializes JSON file to instances."""
+"""
+Contains the FileStorage class
+"""
 
 import json
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
-    """A class used to serialize and deserialize JSON file to instances"""
+    """serializes instances to a JSON file & deserializes back to instances"""
+
+    # string - path to the JSON file
     __file_path = "file.json"
+    # dictionary - empty but will store all objects by <class name>.id
     __objects = {}
 
-    def all(self):
-        """Returns the dictionary __objects"""
-        return type(self).__objects
-    
+    def all(self, cls=None):
+        """returns the dictionary __objects"""
+        if cls is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
+        return self.__objects
+
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
-        type(self).__objects[obj.__class__.__name__ + '.' + str(obj.id)] = obj
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file"""
-        serialized = {}
-        for key, value in type(self).__objects.items():
-            serialized[key] = value.to_dict()
-        with open(type(self).__file_path, mode='w', encoding="UTF-8") as f:
-            for key in serialized.keys():
-                json.dump(serialized, f)
-                f.write("\n")
-
-    def classes(self):
-        """Returns a dictionary of valid classes and their references."""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-        classes = {'BaseModel': BaseModel,
-                    'User': User,
-                    'State': State,
-                    'City': City,
-                    'Amenity': Amenity,
-                    'Place': Place,
-                    'Review': Review}
-        return classes
+        """serializes __objects to the JSON file (path: __file_path)"""
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_objects, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects(Only if the JSON file
-        (__file_path)exists; otherwise, does nothing. if the file doesn't exist
-        no exception is raised)"""
+        """deserializes the JSON file to __objects"""
         try:
-            with open(type(self).__file_path, mode='r', encoding="UTF-8") as f:
-                for line in f:
-                    data = json.loads(line)
-                    from models.base_model import BaseModel
-                    from models.user import User
-                    from models.state import State
-                    from models.city import City
-                    from models.amenity import Amenity
-                    from models.place import Place
-                    from models.review import Review
-                    classes = {'BaseModel': BaseModel,
-                               'User': User,
-                               'State': State,
-                               'City': City,
-                               'Amenity': Amenity,
-                               'Place': Place,
-                               'Review': Review}
-                    for key, value in data.items():
-                        class_name, obj_id = key.split('.')
-                        obj = classes[class_name](**value)
-                        type(self).__objects[key] = obj
-
-        except FileNotFoundError:
+            with open(self.__file_path, 'r') as f:
+                jo = json.load(f)
+            for key in jo:
+                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+        except:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
